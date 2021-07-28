ca42863f6aa4f642daa27cee8bc5e62c50198a7d
Tak-Ohta <sacredfield.tak@gmail.com>
2021-07-28 21:04:43 +0900

    Userモデルのnameカラムとemailカラムにバリデーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210727114418) do
+ActiveRecord::Schema.define(version: 20210728115649) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20210727114418) do
     t.string "annual_income"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
