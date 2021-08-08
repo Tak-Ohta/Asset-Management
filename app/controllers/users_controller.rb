7d21a03520bc716ea3904b119bcf582ecdd93044
Tak-Ohta <sacredfield.tak@gmail.com>
2021-08-08 17:21:59 +0900

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      logged_in @user # ユーザー登録成功後、ログインする
       flash[:success] = "新規登録に成功しました。"
       redirect_to @user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,11 @@ module SessionsHelper
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す
   def logged_in?
-    !@current_user.nil?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
