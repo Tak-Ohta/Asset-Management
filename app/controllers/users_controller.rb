866f63fd3f9e543c4c89cd732bd590b6a61f5c31
Tak-Ohta <sacredfield.tak@gmail.com>
2021-08-09 16:51:29 +0900

    Finish full-fledged-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      logged_in @user # ユーザー登録成功後、ログインする
+      log_in @user # ユーザー登録成功後、ログインする
       flash[:success] = "新規登録に成功しました。"
       redirect_to @user
     else
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation,
-                                    :birthday, :sex, :prefecture)
+                                    :birthday, :sex, :prefecture, :occupation, :marriage,
+                                    :annual_income)
     end
 end
