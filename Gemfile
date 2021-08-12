9fe434f6a687b1f7e661b71f4686455fcd4592f3
Tak-Ohta <sacredfield.tak@gmail.com>
2021-08-12 21:16:32 +0900

    ユーザー一覧ページ作成

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem 'rails', '~> 5.1.6'
 gem 'rails-i18n'
 gem 'bootstrap-sass'
+gem 'faker'
 gem 'jquery-rails'
 gem 'sqlite3'
 gem 'puma', '~> 3.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,232 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+User.create!(name: "Admin User",
+            email: "admin@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1970-01-01",
+            sex: "男性",
+            prefecture: "東京都新宿区",
+            occupation: "会社員（正社員）",
+            marriage: "既婚",
+            annual_income: "600万円以上 〜 800万円未満")
+
+User.create!(name: "一郎丸",
+            email: "sample-1@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "2001-06-10",
+            sex: "男性",
+            prefecture: "神奈川県横浜市",
+            occupation: "大学生・大学院生",
+            marriage: "未婚",
+            annual_income: "200万円未満")
+
+User.create!(name: "豆もやし",
+            email: "sample-2@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1994-10-10",
+            sex: "男性",
+            prefecture: "大阪府大阪市",
+            occupation: "公務員",
+            marriage: "未婚",
+            annual_income: "400万円以上 〜 600万円未満")
+
+User.create!(name: "ロミロミ",
+            email: "sample-3@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1997-07-20",
+            sex: "女性",
+            prefecture: "東京都渋谷区",
+            occupation: "専門学校生・短大生",
+            marriage: "未婚",
+            annual_income: "200万円未満")
+
+User.create!(name: "Rie",
+            email: "sample-4@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1993-11-20",
+            sex: "女性",
+            prefecture: "愛知県名古屋市",
+            occupation: "会社員（正社員）",
+            marriage: "既婚",
+            annual_income: "400万円以上 〜 600万円未満")
+
+User.create!(name: "太郎",
+            email: "sample-5@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1990-04-15",
+            sex: "男性",
+            prefecture: "福岡県北九州市",
+            occupation: "会社員（派遣社員）",
+            marriage: "未婚",
+            annual_income: "400万円以上 〜 600万円未満")
+
+User.create!(name: "かとちゃん",
+            email: "sample-6@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1984-08-15",
+            sex: "男性",
+            prefecture: "石川県小松市",
+            occupation: "会社員（正社員）",
+            marriage: "既婚",
+            annual_income: "600万円以上 〜 800万円未満")
+
+User.create!(name: "みーさん",
+            email: "sample-7@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1989-05-15",
+            sex: "女性",
+            prefecture: "北海道札幌市",
+            occupation: "経営者・会社役員",
+            marriage: "未婚",
+            annual_income: "2,000万円以上")
+
+User.create!(name: "ハナ",
+            email: "sample-8@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1983-09-15",
+            sex: "女性",
+            prefecture: "宮城県仙台市",
+            occupation: "専業主婦・主夫",
+            marriage: "既婚",
+            annual_income: "200万円未満")
+
+User.create!(name: "しのぶパパ",
+            email: "sample-9@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1980-04-01",
+            sex: "男性",
+            prefecture: "東京都調布市",
+            occupation: "会社員（正社員）",
+            marriage: "既婚",
+            annual_income: "600万円以上 〜 800万円未満")
+
+User.create!(name: "サトシ",
+            email: "sample-10@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1974-08-01",
+            sex: "男性",
+            prefecture: "神奈川県相模原市",
+            occupation: "士業（弁護士・公認会計士・税理士等",
+            marriage: "既婚",
+            annual_income: "2,000万円以上")
+
+User.create!(name: "みっきー",
+            email: "sample-11@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1979-05-01",
+            sex: "女性",
+            prefecture: "大阪府大阪市",
+            occupation: "公務員",
+            marriage: "未婚",
+            annual_income: "600万円以上 〜 800万円未満")
+
+User.create!(name: "まりりん",
+            email: "sample-12@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1973-09-01",
+            sex: "女性",
+            prefecture: "東京都足立区",
+            occupation: "会社員（派遣社員）",
+            marriage: "既婚",
+            annual_income: "200万円以上 〜 400万円未満")
+
+User.create!(name: "しょーた",
+            email: "sample-13@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1971-12-10",
+            sex: "男性",
+            prefecture: "愛知県豊田市",
+            occupation: "士業（弁護士・公認会計士・税理士等",
+            marriage: "未婚",
+            annual_income: "800万円以上 〜 1,000万円未満")
+
+User.create!(name: "小野D",
+            email: "sample-14@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1964-08-20",
+            sex: "男性",
+            prefecture: "福岡県福岡市",
+            occupation: "会社員（正社員）",
+            marriage: "既婚",
+            annual_income: "1,000万円以上 〜 2,000万円未満")
+
+User.create!(name: "真衣様",
+            email: "sample-15@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1969-11-20",
+            sex: "女性",
+            prefecture: "石川県金沢市",
+            occupation: "専業主婦・主夫",
+            marriage: "既婚",
+            annual_income: "200万円未満")
+
+User.create!(name: "みるくてぃ",
+            email: "sample-16@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1963-07-20",
+            sex: "女性",
+            prefecture: "京都府京都市",
+            occupation: "自営業・フリーランス",
+            marriage: "既婚",
+            annual_income: "1,000万円以上 〜 2,000万円未満")
+
+User.create!(name: "Kenken",
+            email: "sample-17@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1961-04-05",
+            sex: "男性",
+            prefecture: "新潟県新潟市",
+            occupation: "経営者・会社役員",
+            marriage: "既婚",
+            annual_income: "2,000万円以上")
+
+User.create!(name: "シモンヌ",
+            email: "sample-18@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1955-08-05",
+            sex: "男性",
+            prefecture: "東京都大田区",
+            occupation: "会社員（契約社員）",
+            marriage: "未婚",
+            annual_income: "400万円以上 〜 600万円未満")
+
+User.create!(name: "すずはな",
+            email: "sample-19@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1959-05-05",
+            sex: "女性",
+            prefecture: "神奈川県茅ヶ崎市",
+            occupation: "定年退職",
+            marriage: "既婚",
+            annual_income: "200万円未満")
+
+User.create!(name: "Peach",
+            email: "sample-20@email.com",
+            password: "password",
+            password_confirmation: "password",
+            birthday: "1954-09-05",
+            sex: "女性",
+            prefecture: "埼玉県さいたま市",
+            occupation: "パート・アルバイト",
+            marriage: "既婚",
+            annual_income: "200万円未満")
